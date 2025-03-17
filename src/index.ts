ed8ffa3949308350b1ce8226cea7e9c9704224de
Yordan Lazarov <yordanlazarov@Dakata-765.local>
2025-03-17 08:47:04 +0200

    Export Config & Auth types

--- src/index.ts
+++ src/index.ts
@@ -4,12 +4,14 @@ import Requester, {
     convertObjectToFormData,
     convertURLSearchParamsToObject
 } from "@src/Requester";
+import Config from "@src/type/Config";
 import InterceptEvent from "@src/enum/InterceptEvent";
 import Method from "@src/enum/Method";
 import RequestBodyType from "@src/enum/RequestBodyType";
 import BearerToken from "@src/component/authorization/BearerToken";
 import BasicAuth from "@src/component/authorization/BasicAuth";
 import APIKey from "@src/component/authorization/APIKey";
+import AuthorizationInterface from "@src/component/authorization/AuthorizationInterface";
 
 export {
     Requester as default,
@@ -24,3 +26,8 @@ export {
     BasicAuth,
     APIKey
 };
+
+export type {
+    Config,
+    AuthorizationInterface
+}
